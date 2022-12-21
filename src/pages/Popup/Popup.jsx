d4b2c345b044a6447b3df3878d2be11633c04c21
Thomas <me@hvs.cx>
2022-12-21 02:23:27 +0100

    add option component into popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, {useEffect} from 'react';
 import './Popup.css';
+import Options from "../Options/Options";
 
 const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        document.body.classList.add(
+            'relative',
+            'w-96',
+            'h-10',
+        );
+    }, []);
+
+  return <Options title={'TCD Torrent'} />;
 };
 
 export default Popup;
